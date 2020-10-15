2d3352b9614b39ee5e101121896e263a122a9d77
Fajar AM <somabangsa@gmail.com>
2020-10-15 13:54:07 +0700

    update url

--- src/constant/Url.js
+++ src/constant/Url.js
@@ -1,7 +1,7 @@
 
 const devMode = document.getElementById("rootPath").value == "${contextPath}";
 const rootValue = devMode ?
-     "/universal-good-shop/" : document.getElementById("rootPath").value;
+     "/universal-good-shop/" : document.getElementById("rootPath").value+"/";
 
 export const contextPath = function(){
     return devMode? "http://localhost:8080".concat(rootValue):rootValue;
